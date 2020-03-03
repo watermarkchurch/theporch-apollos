c63e4c4d262e133e697e625b1d22f4ae8185e83a
Jordan Wade <jjwade31@gmail.com>
2020-03-03 14:29:14 -0600

    Remove more auth stuff and route to landing screen and onboarding

--- apolloschurchapp/src/index.js
+++ apolloschurchapp/src/index.js
@@ -8,15 +8,12 @@ import { BackgroundView, withTheme } from '@apollosproject/ui-kit';
 import Passes from '@apollosproject/ui-passes';
 import { MapViewConnected as Location } from '@apollosproject/ui-mapview';
 import { MediaPlayer } from '@apollosproject/ui-media-player';
-import Auth, { ProtectedRoute } from '@apollosproject/ui-auth';
 
 import Providers from './Providers';
 import NavigationService from './NavigationService';
 import ContentSingle from './content-single';
 import Event from './event';
 import Tabs from './tabs';
-import PersonalDetails from './user-settings/PersonalDetails';
-import ChangePassword from './user-settings/ChangePassword';
 import LandingScreen from './LandingScreen';
 import UserWebBrowser from './user-web-browser';
 import Onboarding from './ui/Onboarding';
@@ -26,26 +23,11 @@ const AppStatusBar = withTheme(({ theme }) => ({
   backgroundColor: theme.colors.paper,
 }))(StatusBar);
 
-const ProtectedRouteWithSplashScreen = (props) => {
-  const handleOnRouteChange = () => SplashScreen.hide();
-
-  return <ProtectedRoute {...props} onRouteChange={handleOnRouteChange} />;
-};
-
-// Hack to avoid needing to pass emailRequired through the navigator.navigate
-const EnhancedAuth = (props) => <Auth {...props} emailRequired />;
-// 😑
-hoistNonReactStatic(EnhancedAuth, Auth);
-
 const AppNavigator = createStackNavigator(
   {
-    ProtectedRoute: ProtectedRouteWithSplashScreen,
     Tabs,
     ContentSingle,
     Event,
-    Auth: EnhancedAuth,
-    PersonalDetails,
-    ChangePassword,
     Location,
     Passes,
     UserWebBrowser,
@@ -53,7 +35,7 @@ const AppNavigator = createStackNavigator(
     LandingScreen,
   },
   {
-    initialRouteName: 'ProtectedRoute',
+    initialRouteName: 'LandingScreen',
     mode: 'modal',
     headerMode: 'screen',
   }
