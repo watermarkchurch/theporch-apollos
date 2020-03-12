b5365f85c1bb0302dfaf66c64a76bfd05d14339d
Jordan Wade <jjwade31@gmail.com>
2020-03-12 16:06:05 -0500

    Add schedule to tabs

--- ThePorch/src/tabs/index.js
+++ ThePorch/src/tabs/index.js
@@ -5,12 +5,14 @@ import TabBar from './tabBar';
 import Connect from './connect';
 import Home from './home';
 import Discover from './discover';
+import Schedule from './schedule';
 
 const TabNavigator = createBottomTabNavigator(
   {
     Home,
     Discover,
     Connect,
+    Schedule,
   },
   {
     tabBarComponent: TabBar,
