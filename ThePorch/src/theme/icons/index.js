c7706ef8f9f4bdcbf81cd75a75148587ac250d1d
Jordan Wade <jjwade31@gmail.com>
2020-04-06 16:45:34 -0500

    Add custom icons

--- ThePorch/src/theme/icons/index.js
+++ ThePorch/src/theme/icons/index.js
@@ -1,2 +1,7 @@
-/* Export your custom icon components here. */
-export {};
+export AwakenAlt from './AwakenAlt';
+export ConnectAlt from './ConnectAlt';
+export DiscoverAlt from './DiscoverAlt';
+// export FavoriteAlt from './FavoriteAlt';
+export HomeAlt from './HomeAlt';
+export LocationAlt from './LocationAlt';
+export TimeAlt from './TimeAlt';
