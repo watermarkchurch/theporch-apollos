b5454909104ec72f4c01596ddeed9f1c8be77e52
Jordan Wade <jjwade31@gmail.com>
2020-04-06 16:47:24 -0500

    Implement tab bar to use two color icons

--- ThePorch/src/tabs/discover/index.js
+++ ThePorch/src/tabs/discover/index.js
@@ -18,7 +18,7 @@ const DiscoverNavigator = createStackNavigator(
       headerTintColor: screenProps.headerTintColor,
       headerTitleStyle: screenProps.headerTitleStyle,
     }),
-    navigationOptions: { tabBarIcon: tabBarIcon('sections') },
+    navigationOptions: { tabBarIcon: tabBarIcon('DiscoverAlt') },
   }
 );
 
