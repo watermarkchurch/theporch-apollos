1d0c640347b207970282696969079bf2a98a0f21
Jordan Wade <jjwade31@gmail.com>
2020-04-07 13:32:15 -0500

    Add favorite icon

--- ThePorch/src/theme/icons/index.js
+++ ThePorch/src/theme/icons/index.js
@@ -1,7 +1,7 @@
 export AwakenAlt from './AwakenAlt';
 export ConnectAlt from './ConnectAlt';
 export DiscoverAlt from './DiscoverAlt';
-// export FavoriteAlt from './FavoriteAlt';
+export FavoriteAlt from './FavoriteAlt';
 export HomeAlt from './HomeAlt';
 export LocationAlt from './LocationAlt';
 export TimeAlt from './TimeAlt';
