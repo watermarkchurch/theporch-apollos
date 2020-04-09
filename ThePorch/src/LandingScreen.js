189aa920b8d5588ad90ae78fa68c9d9372aabcbd
Conrad VanLandingham <conrad@differential.com>
2020-04-09 12:44:07 -0500

    Onboarding (#26)
    * style the LandingScreen
    
    * wire up onboarding notifications screen
    
    * mysterious pod changes

--- ThePorch/src/LandingScreen.js
+++ ThePorch/src/LandingScreen.js
@@ -1,22 +1,58 @@
 import React from 'react';
-import { styled, GradientOverlayImage } from '@apollosproject/ui-kit';
+import { Image, StyleSheet } from 'react-native';
 
-import ApollosLandingScreen from './ui/LandingScreen';
+import {
+  styled,
+  withTheme,
+  Icon,
+  H1,
+  H4,
+  BodyText,
+  PaddedView,
+  BackgroundView,
+  ThemeMixin,
+} from '@apollosproject/ui-kit';
 
-const FullScreenImage = styled({
-  resizeMode: 'cover',
-  position: 'absolute',
-})(GradientOverlayImage);
+import { Slide } from '@apollosproject/ui-onboarding';
+
+const Content = styled({
+  flex: 1,
+  justifyContent: 'center',
+  alignItems: 'center',
+  textAlign: 'center',
+})(PaddedView);
+
+const BrandIcon = withTheme(({ theme }) => ({
+  source: require('./logo.png'),
+  resizeMode: 'contain',
+  style: {
+    height: theme.sizing.baseUnit * 8,
+    width: '70%',
+  },
+}))(Image);
+
+const BackgroundImage = styled({
+  position: 'absolute', top: 0, left: 0, width: '100%', height: '100%',
+})(Image);
+
+const SubHeading = styled({
+  textAlign: 'center',
+})(BodyText);
 
 const LandingScreen = ({ navigation }) => (
-  <ApollosLandingScreen
-    onPressPrimary={() => navigation.push('Onboarding')}
-    textColor={'white'}
-    BackgroundComponent={
-      <FullScreenImage source={'https://picsum.photos/375/812/?random'} />
-    }
-    primaryNavText={"Let's go!"}
-  />
+  <ThemeMixin mixin={{ type: 'dark' }}>
+    <BackgroundView>
+      <Slide primaryNavText={"Come On In!"} onPressPrimary={() => navigation.push('Onboarding')} scrollEnabled={false}>
+        <BackgroundImage source={require('./splash.png')} resizeMode="cover" />
+        <Content>
+          <BrandIcon />
+          <SubHeading>
+            A weekly gathering of thousands of 20 and 30 somethings in Dallas and all over. It’s kind of like church, but not as you know it.
+          </SubHeading>
+        </Content>
+      </Slide>
+    </BackgroundView>
+  </ThemeMixin>
 );
 
 LandingScreen.navigationOptions = {
--- ThePorch/src/ui/Onboarding/index.js
+++ ThePorch/src/ui/Onboarding/index.js
@@ -6,16 +6,11 @@ import {
   RESULTS,
 } from 'react-native-permissions';
 import {
-  GradientOverlayImage,
   styled,
-  BackgroundView,
+  ThemeMixin,
 } from '@apollosproject/ui-kit';
 import {
   AskNotificationsConnected,
-  AskNameConnected,
-  FeaturesConnected,
-  AboutYouConnected,
-  LocationFinderConnected,
   OnboardingSwiper,
 } from '@apollosproject/ui-onboarding';
 import AsyncStorage from '@react-native-community/async-storage';
@@ -23,91 +18,47 @@ import AsyncStorage from '@react-native-community/async-storage';
 import { resetAction } from '../../NavigationService';
 import { useOnboardDispatch, hideOnboarding } from '../../OnboardProvider';
 
-const FullscreenBackgroundView = styled({
-  position: 'absolute',
-  width: '100%',
-  height: '100%',
-})(BackgroundView);
+import BackgroundTexture from '../BackgroundTexture';
 
-const StyledGradient = styled({
-  maxHeight: '40%',
-})(GradientOverlayImage);
+import AskNotifications from './AskNotifications'
+
 
 function Onboarding({ navigation }) {
   const dispatch = useOnboardDispatch();
 
   useEffect(() => {
     AsyncStorage.setItem('hideOnboard', 'true');
-    dispatch(hideOnboarding());
   }, []);
 
   return (
-    <>
-      <FullscreenBackgroundView />
-      <OnboardingSwiper>
-        {({ swipeForward }) => (
-          <>
-            <AskNameConnected onPressPrimary={swipeForward} />
-            <FeaturesConnected
-              onPressPrimary={swipeForward}
-              BackgroundComponent={
-                <StyledGradient
-                  source={'https://picsum.photos/640/640/?random'}
-                />
-              }
-            />
-            <AboutYouConnected
-              onPressPrimary={swipeForward}
-              BackgroundComponent={
-                <StyledGradient
-                  source={'https://picsum.photos/640/640/?random'}
-                />
-              }
-            />
-            <LocationFinderConnected
-              onPressPrimary={swipeForward}
-              onNavigate={() => {
-                navigation.navigate('Location');
-              }}
-              BackgroundComponent={
-                <StyledGradient
-                  source={'https://picsum.photos/640/640/?random'}
-                />
-              }
-            />
-            <AskNotificationsConnected
-              onRequestPushPermissions={(update) => {
-                checkNotifications().then((checkRes) => {
-                  if (checkRes.status === RESULTS.DENIED) {
-                    requestNotifications(['alert', 'badge', 'sound']).then(
-                      () => {
-                        update();
-                      }
-                    );
-                  } else {
-                    openSettings();
-                  }
-                });
-              }}
-              onPressPrimary={() =>
-                navigation.dispatch(
-                  resetAction({
-                    navigatorName: 'Tabs',
-                    routeName: 'Home',
-                  })
-                )
-              }
-              primaryNavText={'Finish'}
-              BackgroundComponent={
-                <StyledGradient
-                  source={'https://picsum.photos/640/640/?random'}
-                />
-              }
-            />
-          </>
-        )}
-      </OnboardingSwiper>
-    </>
+    <ThemeMixin mixin={{ type: 'dark' }}>
+      <BackgroundTexture>
+        <OnboardingSwiper>
+          {({ swipeForward }) => (
+            <>
+              <AskNotificationsConnected
+                onRequestPushPermissions={(update) => {
+                  checkNotifications().then((checkRes) => {
+                    if (checkRes.status === RESULTS.DENIED) {
+                      requestNotifications(['alert', 'badge', 'sound']).then(
+                        () => {
+                          update();
+                        }
+                      );
+                    } else {
+                      openSettings();
+                    }
+                  });
+                }}
+                onPressPrimary={() => dispatch(hideOnboarding())}
+                Component={AskNotifications}
+                primaryNavText={'Finish'}
+              />
+            </>
+          )}
+        </OnboardingSwiper>
+      </BackgroundTexture>
+    </ThemeMixin>
   );
 }
 
