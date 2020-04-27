09f95e39555029b032d85878a586ac229604e8da
Vincent Wilson <vince@classyh.at>
2020-04-27 16:22:58 -0400

    Finalized livestream setup

--- apollos-church-api/src/data/livestream/index.js
+++ apollos-church-api/src/data/livestream/index.js
@@ -20,27 +20,30 @@ class dataSource extends RESTDataSource {
   getWebviewUrl({ current_event, next_event }) {
     const url = current_event?.embed_code || next_event?.embed_code;
     if (url) {
-      console.log(url);
       return /src="(.*?)"/.exec(url)[1];
     }
     return null;
   }
 
+  async contentItemForEvent({ current_event, next_event }) {
+    const url = current_event?._links?.message || next_event?._links?.message;
+    const { message } = await this.get(url);
+    return message;
+  }
+
   async getLiveStreams() {
-    const { ContentItem } = this.context.dataSources;
     // This logic is a little funky right now.
     // The follow method looks at the sermon feed and the `getLiveStream` on this module
     // If we have data in the sermon feed, and the `getLiveStream.isLive` is true
     // this returns an array of livestreams
     const { streams } = await this.get('', { target: 'the_porch' });
-    console.log(JSON.stringify(streams, null, 2));
     return streams.map((stream) => ({
       isLive: !!stream.current_event,
       eventStartTime:
         stream.current_event?.starts_at || stream.next_event?.starts_at,
       media: this.getMediaUrl(stream),
       webViewUrl: this.getWebviewUrl(stream),
-      // contentItem:
+      contentItem: this.contentItemForEvent(stream),
     }));
   }
 }
