c3a2704f3ddc131462fde272f2cde13562aea1a9
Conrad VanLandingham <conrad@differential.com>
2020-04-28 15:55:17 -0500

    Add speakers content channel (#52)

---
+++ apollos-church-api/src/data/WCCSpeaker.js
@@ -0,0 +1,115 @@
+import gql from 'graphql-tag';
+import {
+  createGlobalId,
+  createCursor,
+  parseCursor,
+} from '@apollosproject/server-core';
+import { RESTDataSource } from 'apollo-datasource-rest';
+
+export class dataSource extends RESTDataSource {
+  getFromId = (id) => ({ name: id });
+
+  paginate = async ({ pagination: { after, first = 5 } }) => {
+    const indice = this.context.dataSources.Search.indice(
+      this.context.dataSources.Search.messagesIndex
+    );
+    let {
+      facets: { 'speakers.speaker_name': speakers },
+    } = await indice.search({
+      query: '',
+      facets: ['speakers.speaker_name'],
+      filters: 'ministries:"The Porch"',
+    });
+
+    speakers = Object.keys(speakers);
+
+    // parse the incoming cursor
+    if (after) {
+      const parsed = parseCursor(after);
+      if (parsed && parsed.speakerName) {
+        const afterIndex = speakers.indexOf(parsed.speakerName);
+        if (afterIndex) speakers = speakers.slice(afterIndex + 1);
+      } else {
+        throw new Error(`An invalid 'after' cursor was provided: ${after}`);
+      }
+    }
+
+    // limit to pagination
+    speakers = speakers.slice(0, first);
+
+    // build the edges - translate speakers name array to { edges: [{ node, cursor }] } format
+    const edges = (speakers || []).map((name) => ({
+      node: { name, __typename: 'WCCSpeaker' },
+      cursor: createCursor({ speakerName: name }),
+    }));
+
+    return {
+      edges,
+      getTotalCount: () => speakers.length,
+    };
+  };
+}
+
+export const schema = gql`
+  type WCCSpeaker implements Node & ContentItem {
+    id: ID!
+    title(hyphenated: Boolean): String
+    coverImage: ImageMedia
+    htmlContent: String
+    summary: String
+    childContentItemsConnection(
+      first: Int
+      after: String
+    ): ContentItemsConnection
+    siblingContentItemsConnection(
+      first: Int
+      after: String
+    ): ContentItemsConnection
+    images: [ImageMedia]
+    videos: [VideoMedia]
+    audios: [AudioMedia]
+    parentChannel: ContentChannel
+    theme: Theme
+  }
+`;
+
+export const resolver = {
+  WCCSpeaker: {
+    id: ({ name }, args, context, { parentType }) =>
+      createGlobalId(name, parentType.name),
+    title: ({ name }) => name,
+    summary: () => null,
+    htmlContent: () => '',
+    coverImage: async ({ name }, args, { dataSources }) => {
+      const speaker = await dataSources.WCCMessage.getSpeakerByName({ name });
+      if (speaker?.image) {
+        return { sources: [{ uri: speaker.image }] };
+      }
+      return null;
+    },
+    sharing: ({ name }) => ({
+      url: null,
+      title: 'Share via ...',
+      message: `${name}`,
+    }),
+    parentChannel: (input, args, { dataSources }) =>
+      dataSources.ContentChannel.getSpeakersChannel(), // TODO
+    childContentItemsConnection: async (
+      { name },
+      pagination,
+      { dataSources }
+    ) => {
+      const results = await dataSources.Search.byPaginatedQuery({
+        ...pagination,
+        index: dataSources.Search.messagesPopularIndex,
+        filters: `ministries:"The Porch" AND speakers.speaker_name:"${name}"`,
+      });
+      return {
+        edges: results.map(({ cursor, ...result }) => ({
+          node: result,
+          cursor,
+        })),
+      };
+    },
+  },
+};
