02a7ea4e16e383afeb7066b8d14bad01f705b3c1
Vincent Wilson <vincent@differential.com>
2020-10-23 12:20:59 -0400

    Fix android build issue

---
+++ ThePorch/react-native.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  project: {
+    ios: {},
+    android: {}, // grouped into "project"
+  },
+  assets: ['./assets/fonts/'], // stays the same
+  dependencies: {
+    '@metarouter/analytics-react-native': {
+      platforms: {
+        android: null, // disable Android platform, other platforms will still autolink if provided
+        ios: null,
+      },
+    },
+  },
+};
