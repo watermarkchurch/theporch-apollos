1258203ca4be4ba6b7c089f6d5d7622e7c0ed3bc
Christina Megura <cmegura@gmail.com>
2020-11-03 14:38:30 -0500

    Different approach

--- ThePorch/src/ui/ActionListFeatureConnected/index.js
+++
@@ -1,6 +0,0 @@
-import ActionListFeatureConnected from './ActionListFeatureConnected';
-
-export ActionListFeature from './ActionListFeature';
-export GET_ACTION_LIST_FEATURE from './getActionListFeature';
-
-export default ActionListFeatureConnected;
