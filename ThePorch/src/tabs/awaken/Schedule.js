5ac6eb6fe279f214296704a469b62a766ef73e88
Conrad VanLandingham <conrad@differential.com>
2021-08-09 12:51:11 -0500

    Awaken 2021 (#140)
    * fix pods
    
    * add awaken tab

--- ThePorch/src/tabs/awaken/Schedule.js
+++ ThePorch/src/tabs/awaken/Schedule.js
@@ -1,26 +1,131 @@
 import React, { PureComponent } from 'react';
 import { Query } from 'react-apollo';
-import { Platform, StatusBar, ScrollView } from 'react-native';
+import gql from 'graphql-tag';
+import { SectionList, Image, SafeAreaView } from 'react-native';
+import ApollosConfig from '@apollosproject/config';
+
 import {
   BackgroundView,
-  // TabView,
-  ActivityIndicator,
-  H2,
   ThemeMixin,
   ErrorCard,
+  H4,
+  styled,
+  HorizontalTileFeed,
+  PaddedView,
+  TouchableScale,
 } from '@apollosproject/ui-kit';
 
+import { HorizontalContentCardConnected } from '@apollosproject/ui-connected';
+
+import horizontalContentCardMapper from '../../ui/horizontalContentCardMapper';
+import Label from '../../ui/LabelText';
+
 import { ReactNavigationStyleReset } from '../../ui/SearchInputHeader';
+import ScheduleItem from '../../ui/ScheduleItem';
 
 import headerOptions from '../headerOptions';
-import AppStateRefetch from '../../ui/AppStateRefetch';
-import GET_DAYS from './getDays';
-import UpNext from './UpNext';
-import ConferenceCard from './ConferenceCard';
 
-import Day from './Day';
+const getDays = gql`
+  query {
+    conference {
+      announcements {
+        edges {
+          node {
+            ...contentCardFragment
+          }
+        }
+      }
+      days {
+        id
+        title
+        date
+        childContentItemsConnection {
+          edges {
+            node {
+              id
+              title
+              summary
+              htmlContent
+              childContentItemsConnection {
+                pageInfo {
+                  startCursor
+                }
+              }
+              ... on Event {
+                startTime
+                endTime
+              }
+              ... on Breakouts {
+                startTime
+                endTime
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  ${ApollosConfig.FRAGMENTS.CONTENT_CARD_FRAGMENT}
+`;
 
-class Schedule extends PureComponent {
+const SectionHeader = styled(({ theme }) => ({
+  backgroundColor: theme.colors.background.paper,
+  color: theme.colors.text.secondary,
+  paddingHorizontal: theme.sizing.baseUnit,
+  paddingVertical: theme.sizing.baseUnit / 2,
+}))(H4);
+
+const loadingStateObject = {
+  id: 'fake_id',
+  title: '',
+  coverImage: [],
+};
+
+const HeaderImage = styled({
+  height: 88,
+  marginTop: 16,
+  marginBottom: 16,
+  paddingLeft: 16,
+  paddingRight: 16,
+  aspectRatio: 1920 / 1080,
+  resizeMode: 'contain',
+  alignSelf: 'center',
+})(Image);
+
+const ScheduleHeader = ({ announcements, isLoading, navigation }) => (
+  <>
+    <HeaderImage source={require('./hero.png')} />
+    <PaddedView vertical={false}>
+      <Label>Announcements</Label>
+    </PaddedView>
+    <HorizontalTileFeed
+      content={announcements}
+      loadingStateObject={loadingStateObject}
+      renderItem={({ item }) => (
+        <TouchableScale
+          onPress={() => {
+            navigation.push('ContentSingle', {
+              itemId: item.id,
+            });
+          }}
+        >
+          <HorizontalContentCardConnected
+            Component={horizontalContentCardMapper}
+            contentId={item.id}
+            isLoading={isLoading}
+          />
+        </TouchableScale>
+      )}
+      keyExtractor={(a) => a.id}
+      isLoading={isLoading}
+    />
+    <PaddedView>
+      <Label>Schedule</Label>
+    </PaddedView>
+  </>
+);
+
+const Schedule = class Schedule extends PureComponent {
   static navigationOptions = ({ screenProps }) => ({
     title: 'Awaken',
     ...headerOptions,
@@ -35,56 +140,69 @@ class Schedule extends PureComponent {
     ),
   });
 
-  state = {
-    index: 0,
+  renderSchedule = ({ loading, data, refetch }) => {
+    const sections = (data?.conference?.days || [])
+      .slice()
+      .sort((a, b) => new Date(a.date) - new Date(b.date))
+      .map((day) => ({
+        title: day?.title,
+        data: day?.childContentItemsConnection?.edges.map(({ node }) => node),
+      }));
+
+    const announcements = (data?.conference?.announcements?.edges || [])
+      .slice()
+      .map(({ node }) => node);
+
+    const renderItem = ({ item }) => (
+      <ScheduleItem
+        onPress={() =>
+          this.props.navigation.navigate('ContentSingle', {
+            itemId: item.id,
+            transitionKey: item.transitionKey,
+          })
+        }
+        {...item}
+      />
+    );
+
+    const renderSectionHeader = ({ section }) => (
+      <SectionHeader>{section.title}</SectionHeader>
+    );
+
+    return (
+      <SectionList
+        refreshing={loading}
+        onRefresh={refetch}
+        style={{ flex: 1 }}
+        sections={sections}
+        renderItem={renderItem}
+        renderSectionHeader={renderSectionHeader}
+        ListHeaderComponent={
+          <ScheduleHeader
+            announcements={announcements}
+            navigation={this.props.navigation}
+            isLoading={loading}
+          />
+        }
+      />
+    );
   };
-
-  componentDidMount() {
-    this._navListener = this.props.navigation.addListener('didFocus', () => {
-      StatusBar.setBarStyle('light-content');
-      Platform.OS === 'android'
-        ? StatusBar.setBackgroundColor('#EF5E24')
-        : null; // todo: don't hard-code color value
-    });
-  }
-
-  componentWillUnmount() {
-    this._navListener.remove();
-  }
 
   render() {
     return (
       <BackgroundView>
-        <Query query={GET_DAYS} fetchPolicy="cache-and-network">
-          {({
-            loading,
-            data: { conference: { days = [] } = {} } = {},
-            error,
-            refetch,
-          }) => {
-            if (loading && !days.length) return <ActivityIndicator />;
-            if (error) return <ErrorCard error={error} />;
+        <SafeAreaView style={{ flex: 1 }}>
+          <Query query={getDays} fetchPolicy="cache-and-network">
+            {({ loading, data, error, refetch }) => {
+              if (error) return <ErrorCard error={error} />;
 
-            return (
-              <>
-                <AppStateRefetch refetch={refetch} />
-                <ScrollView>
-                  <ConferenceCard />
-                  {/* <UpNext />
-                  {days.map((day) => (
-                    <>
-                      <H2>{day.title}</H2>
-                      <Day id={day.id} />
-                    </>
-                  ))} */}
-                </ScrollView>
-              </>
-            );
-          }}
-        </Query>
+              return this.renderSchedule({ loading, data, error, refetch });
+            }}
+          </Query>
+        </SafeAreaView>
       </BackgroundView>
     );
   }
-}
+};
 
 export default Schedule;
