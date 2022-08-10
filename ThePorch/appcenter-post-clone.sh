a157bd9f5f54b62441bf39de49d50a4a8757cc02
Vincent Wilson <vincent@differential.com>
2022-08-10 11:51:38 -0400

    Update appcenter-post-clone.sh

--- ThePorch/appcenter-post-clone.sh
+++ ThePorch/appcenter-post-clone.sh
@@ -7,8 +7,8 @@ grep -o '\$.*' .env.production | sed 's/\$\(.*\)/\1/' | xargs -I {} sh -c "sed -
 # Make sure ReactNativeConfig picks up values from prod env file.
 cp .env.production .env
 
-echo "Uninstalling all CocoaPods versions"
-sudo gem uninstall cocoapods --all --executables
+#echo "Uninstalling all CocoaPods versions"
+#sudo gem uninstall cocoapods --all --executables
 
 COCOAPODS_VER=`sed -n -e 's/^COCOAPODS: \([0-9.]*\)/\1/p' ios/Podfile.lock`
 
